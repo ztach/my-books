bf0c6e1c885fe4ad79a59ff0727e6751dacc8756
Staszek Olejnik <ztach@poczta.onet.pl>
2020-03-03 14:51:25 +0100

    formularz kontaktowy cz 3

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { graphql } from 'gatsby';
+import Image from "gatsby-image";
 import styled from "styled-components";
-
 import ContactPreview from "../components/ContactPreview/ContactPreview";
 
 const StyledWrapper = styled.div`
@@ -9,12 +10,43 @@ const StyledWrapper = styled.div`
   text-align: justify;
 `;
 
-const ContactPage = () => {
+const ImageWrapper = styled(Image)`
+  position: absolute !important;
+  top: 0;
+  right: 0;
+  width: 42%;
+  height: 100vh;
+  object-fit: cover;
+  background-color: ${({ theme }) => theme.index};
+  z-index: 100;
+`;
+
+const ContactPage = ({ data: { datoCmsListgraph:{ picture } } }) => {
+  
   return (
     <StyledWrapper>
       <ContactPreview />
+      
+      <ImageWrapper fluid={picture.fluid} />
     </StyledWrapper>
+
   );
 };
+
+
+export const query = graphql`
+ {
+  datoCmsListgraph(isactive: {eq: true}) {
+    title
+    slug
+    isactive
+    picture {
+        fluid(maxWidth: 800, maxHeight: 1200) {
+          ...GatsbyDatoCmsFluid_tracedSVG
+        }
+    }
+  }
+ }
+ `;
 
 export default ContactPage;
