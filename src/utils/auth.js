90921ff2a9e45cea077520fcdaa7f437dec71dc1
Staszek Olejnik <ztach@poczta.onet.pl>
2020-05-05 20:02:31 +0200

    autoryzacja2

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,10 +9,10 @@ const setUser = user => (window.localStorage.gatsbyUser = JSON.stringify(user));
 
 const auth = isBrowser
   ? new auth0.WebAuth({
-      domain: process.env.GATSBY_AUTH0_DOMAIN,
-      clientID: process.env.GATSBY_AUTH0_CLIENTID,
-      redirectUri: process.env.GATSBY_AUTH0_CALLBACK,
-      audience: process.env.GATSBY_AUTH0_AUDIENCE,
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENTID,
+      redirectUri: process.env.AUTH0_CALLBACK,
+      audience: process.env.AUTH0_AUDIENCE,
       responseType: "token id_token",
       scope: "openid profile email"
     })
@@ -68,13 +68,13 @@ const setSession = (cb = () => {}) => (err, authResult) => {
     const { nickname, name, picture } = user;
 
     navigate("/contact");
-    return setUser({
+    setUser({
       isLoggedIn: true,
       nickname,
       name,
       logo: picture
     });
-    //cb();
+    cb();
   }
 };
 
